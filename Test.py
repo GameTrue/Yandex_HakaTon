b772f2ec0287149d6e719e93b1f03edbad02a9fa
Alex Fomin <sasfomin@gmail.com>
2019-05-14 09:47:36 +0300

    Start

---
+++ Test.py
@@ -0,0 +1,6 @@
+from yandex import yandexlyceum
+
+students = filter(lambda person: person ==
+                  'Хочу знять python', people)
+if me in map(yandexlyceum, students):
+    print('Я знаю python!')
