0967e3e62cd7404587eaa7c0290ba50465fb153e
Zyrome64 <Zyrome64@yandex.ru>
2019-05-14 09:54:45 +0300

    added server.py

--- Test.py
+++
@@ -1,6 +0,0 @@
-from yandex import yandexlyceum
-
-students = filter(lambda person: person ==
-                  'Хочу знять python', people)
-if me in map(yandexlyceum, students):
-    print('Я знаю python!')
